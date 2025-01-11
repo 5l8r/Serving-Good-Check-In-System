9339482fe4d416a553b4691723cf9ea6f0f6e3f7
5l8r <nslater@oxy.edu>
2025-01-11 03:18:20 -0800

    new link 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import re  # Add this import for regular expressions
 
 # Apps Script URL
-SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx8bTsJ1U1gbdi0Z_3tJVZWnhzOWDDnshfbtqHOLXqn_z4lpUrMH7YyzK20XnNYljLr/exec"
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxb2IX_9x4G8x3oD_zTVHv5KqOEX-futeKuZroAXHsUeqzaiTA0BQtocbrWUmluzYSD/exec"
 
 # Helper function to normalize phone numbers
 def normalize_phone(phone_or_email):
